ef0472baaba4a3af7cada1f57f8555e2ef77ea69
willian assuncao <willian.rafael23@bol.com.br>
2022-10-07 21:50:22 -0300

    Criacao do componente principal para o projeto

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Calculadora from './Components/calculadora/calculadora';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Calculadora />
   </React.StrictMode>
 );
 
